74c0a444cc2ff11c85057801555bef95535ee6d3
FreedomOutlines <147484609+Element0101@users.noreply.github.com>
2026-01-22 11:15:16 +0100

    Solve exercises

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let word = str;
+    if (num > 0) {
+        for (let i = 0; i < num - 1; i++) {
+            word += str;
+        }
+    }else if (num < 0) {
+        return "ERROR";
+    }
+    else{
+        word = "";
+    }
+    return word;
 };
 
 // Do not edit below this line
